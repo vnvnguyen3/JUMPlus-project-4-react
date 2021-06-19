71cc69b9ca7dcf06dbad109775685269f727a941
Vincent Nguyen <vnvnguyen3@yahoo.com>
2021-06-19 11:00:09 -0700

    fixed customer info

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,10 +7,10 @@ class Info extends Component {
     render(){
         return (
             <div>
-                <h2>{this.props.user.user.firstName} {this.props.user.user.lastName}</h2>
-                <p>User Id: {this.props.user.user.userId} </p>
-                <p>Your account currently has ${this.props.user.user.amount}</p>
-                <p>Address: {this.props.user.user.address}</p>
+                <h2>{this.props.user.firstName} {this.props.user.lastName}</h2>
+                <p>User Id: {this.props.user.userId} </p>
+                <p>Your account currently has ${this.props.user.amount}</p>
+                <p>Address: {this.props.user.address}</p>
                 <Link to="/options">
                     Go back
                 </Link>
